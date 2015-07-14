834f421fdca03c30e18a3d5ae7d7e3cf80cd4894
nicolapcweek94 <citizenwasp@gmail.com>
2015-07-14 13:21:17 +0200

    Added json configuration file

--- src/yggdrasil.go
+++ src/yggdrasil.go
@@ -4,14 +4,29 @@ import (
 	"fmt"
 	"net"
 	"strconv"
+	"encoding/json"
+	"os"
+	"io"
 )
 
+type Config struct {
+	Services []Service `json:"services"`
+	Irc      IrcConfig `json:"irc"`
+}
+
+type IrcConfig struct {
+	Nick     string    `json:"nick"`
+	Host     string    `json:"host"`
+	Port     int       `json:"port"`
+	Chan     string    `json:"chan"`
+}
+
 type Service struct {
-	Host    string
-	Port    int
-	Address string
-	Proto   string
-	Name    string
+	Host     string    `json:"host"`
+	Port     int       `json:"port"`
+	Address  string
+	Proto    string    `json:"proto"`
+	Name     string    `json:"name"`
 }
 
 func NewService(host string, port int, proto string, name string) (service Service) {
@@ -25,22 +40,33 @@ func NewService(host string, port int, proto string, name string) (service Servi
 	return service
 }
 
-func main() {
-	var services []Service
-	services = append(services, NewService("nwa.xyz", 80, "tcp", "NWA Web Server"))
-	services = append(services, NewService("nerdz.eu", 80, "tcp", "NERDZ Web Server"))
-	services = append(services, NewService("google.com", 80, "tcp", "GOOGLE Web Server"))
-	services = append(services, NewService("wikipedia.org", 80, "tcp", "WIKIPEDIA Web Server"))
-	services = append(services, NewService("microsoft.com", 80, "tcp", "MICROSOFT Web Server"))
+func GetConfig(r io.Reader) (x *Config, err error) {
+	x = new(Config)
+	err = json.NewDecoder(r).Decode(x)
+	return
+}
 
-	for i := range services {
-		fmt.Println(services[i].Proto + " " + services[i].Address)
-		conn, err := net.Dial(services[i].Proto, services[i].Address)
+func main() {
+	conffile, err := os.Open("./config.json")
+	if err != nil {
+		panic(err)
+	}
+
+	config, err := GetConfig(conffile)
+	if err != nil {
+		panic(err)
+	}
+
+	for i := range config.Services {
+		service := config.Services[i]
+		service.Address = service.Host + ":" + strconv.Itoa(service.Port)
+		fmt.Println(service.Proto + " " + service.Address)
+		conn, err := net.Dial(service.Proto, service.Address)
 		if err != nil {
 			panic(err)
 		} else {
 			conn.Close()
-			fmt.Println("Tested " + services[i].Name)
+			fmt.Println("Tested " + service.Name)
 		}
 	}
 }
